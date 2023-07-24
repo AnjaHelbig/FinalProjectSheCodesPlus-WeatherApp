caef58f2df23852a8f5b27fe79bd8974c7345689
Anja Helbig <140140377+Unimaus@users.noreply.github.com>
2023-07-24 19:38:00 +0200

    Adjusting Weather Forecast Loops

--- src/script.js
+++ src/script.js
@@ -50,7 +50,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 7 && index > 0) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
